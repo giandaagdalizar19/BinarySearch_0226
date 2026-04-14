7d9bc7c2d01f4389c5c4dd113130255065afc9fa
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-04-14 10:51:07 +0700

    y

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -21,13 +21,12 @@ void input()
             cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silakan coba lagi.\n";
         }
     }
-    
+
     cout << "\n==============================\n";
     cout << "      Masukkan Elemen Array    \n";
     cout << "==============================\n";
 
     for (int i = 0; i < nPanjang; i++)
-    {
-        cout << "Data ke-" << (i + 1) << " = ";
-    }
+    {}
 }
+
